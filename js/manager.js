3fe77fc4b25f60b913ac75ba3154a6c4efa835c2
nadine-shi <nadine@afterlogic.com>
2016-09-13 17:30:47 +0300

    System - use manager.js files instead of manager-smth.js

--- js/manager.js
+++ js/manager.js
@@ -1,19 +1,21 @@
 'use strict';
 
 
-module.exports = function (oAppData, iUserRole, bPublic) {
+module.exports = function (oAppData) {
 	var
 		_ = require('underscore'),
+		
+		App = require('%PathToCoreWebclientModule%/js/App.js'),
 		
 		Settings = require('modules/%ModuleName%/js/Settings.js'),
 		oSettings = _.extend({}, oAppData[Settings.ServerModuleName] || {}, oAppData['%ModuleName%'] || {}),
 		
-		bAnonimUser = iUserRole === Enums.UserRole.Anonymous
+		bAnonimUser = App.getUserRole() === Enums.UserRole.Anonymous
 	;
 	
 	Settings.init(oSettings);
 	
-	if (!bPublic && bAnonimUser)
+	if (!App.isPublic() && bAnonimUser)
 	{
 		return {
 			/**
