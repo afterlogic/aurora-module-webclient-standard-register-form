f5d2b21a58d35059a8557f7cd68aaf3481db1daf
nadine-shi <nadine@afterlogic.com>
2019-04-26 15:18:35 +0300

    License and other texts improving

--- Module.php
+++ Module.php
@@ -8,6 +8,8 @@
 namespace Aurora\Modules\StandardRegisterFormWebclient;
 
 /**
+ * Displays standard register form with ability to specify user name, account login and password.
+ * 
  * @license https://www.gnu.org/licenses/agpl-3.0.html AGPL-3.0
  * @license https://afterlogic.com/products/common-licensing Afterlogic Software License
  * @copyright Copyright (c) 2019, Afterlogic Corp.
